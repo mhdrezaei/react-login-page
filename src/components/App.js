a3ea78fcd7d090d31d37c2d6b440c3f09aeeb8bf
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-01-27 00:23:05 +0330

    create home and login page and Defining Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
